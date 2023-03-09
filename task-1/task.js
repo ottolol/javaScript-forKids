cb412237d7b8ce491b026b7926266d382f5724bc
haka <alexsveshnik@gmail.com>
2023-03-09 15:08:44 +0300

    добавил решение из книги

--- task-1/task.js
+++ task-1/task.js
@@ -17,3 +17,13 @@ var stringWithoutFirstSimbol = str.slice(1).toLowerCase();
 str = firstSimbol + stringWithoutFirstSimbol;
 
 console.log(str); //Эй, как дела?
+
+
+//решение из книги
+var sillyString = "эЙ, кАК деЛа?";
+var lowerString = sillyString.toLowerCase();
+var firstCharacter = lowerString[0];
+var firstCharacterUpper = firstCharacter.toUpperCase();
+var restOfString = lowerString.slice(1);
+sillyString = firstCharacterUpper + restOfString;
+console.log(sillyString); //Эй, как дела?
