24d495654875fad9e7aeef603ee39fe48accecfd
haka <alexsveshnik@gmail.com>
2023-03-20 21:23:40 +0300

    добавляем несколько элементов при помощи цикла for

--- task-11/app.js
+++ task-11/app.js
@@ -1,3 +1,8 @@
 // var newHeadingText = prompt("Введите новый заголовок:");
 // $("#main-heading").text(newHeadingText);
 $("body").append("<p>Это новый параграф</p>");
+
+for (var i = 0; i < 3; i++) {
+    var hobby = prompt("Назови одно из своих хобби!");
+    $("body").append("<p>" + hobby + "</p>");
+}
