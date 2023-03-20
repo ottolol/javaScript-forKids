e1f9087693f7ddfe3a4d027d957e0db690084330
haka <alexsveshnik@gmail.com>
2023-03-20 21:47:10 +0300

    .fadeIn .fadeOut

--- task-11/app.js
+++ task-11/app.js
@@ -1,8 +1,11 @@
 // var newHeadingText = prompt("Введите новый заголовок:");
 // $("#main-heading").text(newHeadingText);
-$("body").append("<p>Это новый параграф</p>");
+// $("body").append("<p>Это новый параграф</p>");
 
-for (var i = 0; i < 3; i++) {
-    var hobby = prompt("Назови одно из своих хобби!");
-    $("body").append("<p>" + hobby + "</p>");
-}
+// for (var i = 0; i < 3; i++) {
+//     var hobby = prompt("Назови одно из своих хобби!");
+//     $("body").append("<p>" + hobby + "</p>");
+// }
+
+$("h1").fadeOut(3000).fadeIn(2000);
+$("p").text("Этот текст скоро исчезнет").fadeOut(3000);
